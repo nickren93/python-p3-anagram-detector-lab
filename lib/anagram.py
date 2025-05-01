89249b5875abfe716f8f410ce9a7e08d3952be76
Sitong <nickren93@gmail.com>
2025-05-01 13:15:39 -0400

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        original_word_list = sorted([letter for letter in self.word])
+        matching_word_list = []
+        for element in list:
+            if original_word_list == sorted([letter for letter in element]):
+                matching_word_list.append(element)
+        return matching_word_list
